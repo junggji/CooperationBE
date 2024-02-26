b392571daf97baab2129a7d09dc42c4f4c3cbce5
junggji <calrinar@gamil.com>
2024-02-26 13:48:04 +0900

    initial commit

---
+++ src/main/java/com/example/cooperationbe/CooperationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cooperationbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooperationBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CooperationBeApplication.class, args);
+    }
+
+}
